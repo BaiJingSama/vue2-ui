5c7c25845478ecc979cbe71c57253b8d3d1d057d
baijingsama <1303802862@qq.com>
2023-04-13 22:31:44 +0800

    完成readme的撰写

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Button from './src/components/Button'
+import Button from './src/components/button'
 import ButtonGroup from './src/components/button-group'
 import Icon from './src/components/Icon'
 
